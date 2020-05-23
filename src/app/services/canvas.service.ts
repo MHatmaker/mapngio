18bb35ebccd70ff10b16c3305e9871a996b57a81
MHatmaker <Michael.Hatmaker@gmail.com>
2020-05-23 13:44:21 -0700

    reverts to old method of creating and appending new map element

--- src/app/services/canvas.service.ts
+++ src/app/services/canvas.service.ts
@@ -251,19 +251,19 @@ export class CanvasService {
         this.canvases.push(component);
         const mapParent = document.getElementsByClassName('mapcontent')[0];
         // Create a component reference from the component
-        // const componentRef = this.componentFactoryResolver
-        //   .resolveComponentFactory(component)
-        //   .create(MLInjector.injector);
-        const r = this.componentFactoryResolver;
-        const fac = r.resolveComponentFactory(component);
-        // const compRef = fac.create(MLInjector.injector);
-        const compRef = entry.createComponent(fac);
+        const componentRef = this.componentFactoryResolver
+          .resolveComponentFactory(component)
+          .create(MLInjector.injector);
+        // const r = this.componentFactoryResolver;
+        // const fac = r.resolveComponentFactory(component);
+        // // const compRef = fac.create(MLInjector.injector);
+        // const compRef = entry.createComponent(fac);
 
         // Attach component to the appRef so that it's inside the ng component tree
-        // this.appRef.attachView(compRef.hostView);
+        this.appRef.attachView(componentRef.hostView);
 
         // Get DOM element from component
-        const domElem = (compRef.hostView as EmbeddedViewRef<any>)
+        const domElem = (componentRef.hostView as EmbeddedViewRef<any>)
           .rootNodes[0] as HTMLElement;
 
         // Append DOM element to the body
