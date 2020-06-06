92adee6b6cf54756ebcdc77f6d1551a1edd8bd16
MHatmaker <Michael.Hatmaker@gmail.com>
2020-06-05 18:37:27 -0700

    fixes height problem allowing all menus to display and provide item selection

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-news',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public viewCtrl: ModalController) { }
 
   ngOnInit() {}
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
 
 }
