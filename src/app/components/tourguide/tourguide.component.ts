b3684afb42ae48f2271fcd54a22332d245f36c00
MHatmaker <Michael.Hatmaker@gmail.com>
2020-10-10 14:49:56 -0700

    simplified TourGuideComponent with light implementation of ControlValueAccessor works

--- src/app/components/tourguide/tourguide.component.ts
+++ src/app/components/tourguide/tourguide.component.ts
@@ -1,5 +1,5 @@
-import { Component, AfterViewInit, AfterContentInit, forwardRef, ChangeDetectorRef } from '@angular/core';
-import { FormGroup, FormBuilder, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, AfterContentInit, ChangeDetectorRef } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 import { ModalController } from '@ionic/angular';
 import { PusherclientService } from '../../services/pusherclient.service';
@@ -7,14 +7,7 @@ import { PusherclientService } from '../../services/pusherclient.service';
 @Component({
   selector: 'app-tourguide',
   templateUrl: './tourguide.component.html',
-  styleUrls: ['./tourguide.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => TourguideComponent),
-      multi: true
-    }
-  ]
+  styleUrls: ['./tourguide.component.scss']
 })
 export class TourguideComponent implements AfterContentInit, ControlValueAccessor {
   public tourists: IterableIterator<string>;
@@ -27,43 +20,40 @@ export class TourguideComponent implements AfterContentInit, ControlValueAccesso
   ngAfterContentInit() {
     this.pushsvc.touristsUpdated.subscribe((tourClients: IterableIterator<string>) => {
       console.log('got touristsUpdated Event');
-      // console.log(this.tourists);
       console.log(tourClients);
-      this.ref.detectChanges();
       this.tourists = tourClients;
-      // console.log(this.tourists);
-      this.ref.detectChanges();
     });
-    // this.ref.detectChanges();
     this.pushsvc.getTouristList();
-    // this.ref.detectChanges();
   }
   writeValue(): void {
       // this.selectedItems = items || [];
       // this.onChange(this.selectedItems);
-    }
+    console.log('writeValue');
+  }
 
-    registerOnChange(fn: any): void {
-      // this.onChange = fn;
-      console.log('registerOnChange');
-      console.log(fn);
-    }
+  registerOnChange(fn: any): void {
+    // this.onChange = fn;
+    console.log('registerOnChange');
+    console.log(fn);
+  }
 
-    registerOnTouched(fn: any): void {
-      // this.onTouch = fn;
-    }
+  registerOnTouched(fn: any): void {
+    // this.onTouch = fn;
+    console.log('registerOnTouched');
+    console.log(fn);
+  }
   selectTourGuide(t: string) {
     console.log('TourGuide selected is ' + t);
     this.pushsvc.updateCurrentTourGuide(t);
   }
 
   async accept() {
-    console.log('agoitem component accept');
+    console.log('TourguideComponent component accept');
     this.modalCtrl.dismiss();
   }
   cancel() {
         //  this.modalCtrl.dismiss();
-    console.log('agoitem got cancel from agodetail');
+    console.log('agoitem got cancel from TourguideComponent');
     this.modalCtrl.dismiss();
   }
 }
