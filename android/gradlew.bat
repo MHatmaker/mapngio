0a2a303dabc11a74cf68b1752302c2b0ae613aef
MHatmaker <Michael.Hatmaker@gmail.com>
2021-09-05 16:36:19 -0700

    fiddles with android configuration properties...gradle, etc.

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -28,7 +28,6 @@ set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
-set ANDROID_SDK_ROOT /opt/android-sdk
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
